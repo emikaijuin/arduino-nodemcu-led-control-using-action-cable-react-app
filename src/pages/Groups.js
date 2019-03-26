9f0f257b458d11043c4768024204ea7b705a2ca4
Emika Ijuin <emikaijuin@gmail.com>
2019-03-26 15:53:15 +0800

    Create page for light groups

---
+++ src/pages/Groups.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Groups extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>My Lights</h1>
+        <div>Groups Placeholder</div>
+      </div>
+    );
+  }
+}
+
+export default Groups;
