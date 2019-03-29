8b0bdd5132199f49f7308b360cdccfa8d4b9d859
Emika Ijuin <emikaijuin@gmail.com>
2019-03-29 17:09:19 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import GroupIndex from "./pages/GroupsIndex";
-
+import NavBar from "./components/NavBar";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <GroupIndex />
       </div>
     );
