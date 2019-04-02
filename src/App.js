af222bc3b89395cfd2403c7d956104b43f461b83
Emika Ijuin <emikaijuin@gmail.com>
2019-04-02 12:34:57 +0800

    Extract API call to server to separate function, pass ference to function to children in props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import GroupIndex from "./pages/GroupsIndex";
+import GroupsIndex from "./pages/GroupsIndex";
 import NavBar from "./components/NavBar";
+import axios from "axios";
 class App extends Component {
+  state = {};
+  componentDidMount() {
+    this.fetchInformation();
+  }
+
+  fetchInformation = () => {
+    axios
+      .get("http://localhost:3001/groups")
+      .then(result => {
+        this.setState({ ...result.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <GroupIndex />
+        <NavBar
+          lights={this.state.lights}
+          updateGroups={this.fetchInformation}
+        />
+        <GroupsIndex groups={this.state.groups} lights={this.state.lights} />
       </div>
     );
   }
