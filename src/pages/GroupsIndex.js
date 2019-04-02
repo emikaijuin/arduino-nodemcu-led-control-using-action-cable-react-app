68a4f308d0c1d32d4920b5f698029cf4c1488f9b
Emika Ijuin <emikaijuin@gmail.com>
2019-04-02 12:36:59 +0800

    map groups from props, not state

--- src/pages/GroupsIndex.js
+++ src/pages/GroupsIndex.js
@@ -3,24 +3,10 @@ import React, { Component } from "react";
 import Group from "../containers/Group";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
-import Dialog from "@material-ui/core/Dialog";
 
 class GroupsIndex extends Component {
-  state = {};
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:3001/groups")
-      .then(response => {
-        this.setState({ ...this.state, ...response.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
   renderGroups = () =>
-    this.state.groups.map(group => (
+    this.props.groups.map(group => (
       <Group
         id={group.id}
         name={group.name}
