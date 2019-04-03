38bf09d8a2c2e6f0f102aa27aea9a8e99ecc372a
Emika Ijuin <emikaijuin@gmail.com>
2019-04-03 12:01:44 +0800

    render groups from separate functions

--- src/pages/GroupsIndex.js
+++ src/pages/GroupsIndex.js
@@ -2,33 +2,40 @@ import React, { Component } from "react";
 
 import Group from "../containers/Group";
 import Grid from "@material-ui/core/Grid";
-import axios from "axios";
 
 class GroupsIndex extends Component {
+  state = {
+    ungroupedLights: this.props.ungroupedLights,
+    groups: this.props.groups
+  };
+
   renderGroups = () =>
+    // Group component should make API call individually to retrieve information, that way it avoids forcing a re-render after App is updated. Also, then probably don't need to pass down reference to fetchInformation function from App -> Group
     this.props.groups.map(group => (
       <Group
+        key={group.id}
         id={group.id}
         name={group.name}
-        lights={group.lights}
         rgb={group.rgb_state}
+        updateUngroupedLights={this.props.updateUngroupedLights}
       />
     ));
 
-  ungroupedLights = () =>
-    this.props.lights.filter(light => !light.groups.length);
+  renderUngroupedLightsGroup = () => (
+    <Group
+      lights={this.state.ungroupedLights}
+      name="Ungrouped Lights"
+      updateGroups={this.props.updateGroups}
+    />
+  );
 
   render() {
     return (
       <div>
         <div className="groups-container">
           <Grid container spacing={8}>
-            {this.props.groups ? this.renderGroups() : function() {}}
-            <Group
-              lights={this.props.lights ? this.ungroupedLights() : []}
-              name="Ungrouped Lights"
-              id={null}
-            />
+            {this.renderUngroupedLightsGroup()}
+            {this.renderGroups()}
           </Grid>
         </div>
       </div>
